42f8564c07c6cee6e128bfed0cca8b328408e5a3
hyunjine <hyunjin_98_dev@naver.com>
2022-07-05 23:53:17 +0900

    LeakLauncherActivity

--- app/src/main/java/com/hyunjine/petplant/view/post/picture/PictureFragment.kt
+++ app/src/main/java/com/hyunjine/petplant/view/post/picture/PictureFragment.kt
@@ -1,23 +1,52 @@
 package com.hyunjine.petplant.view.post.picture
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.common.internal.service.Common
+import com.hyunjine.petplant.common.TAG
 import com.hyunjine.petplant.common.base.BaseFragment
+import com.hyunjine.petplant.common.util.Test
 import com.hyunjine.petplant.databinding.FragmentPictureBinding
 import com.hyunjine.petplant.view.post.picture.vm.PictureViewModel
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class PictureFragment: BaseFragment<FragmentPictureBinding, PictureViewModel>() {
 
+    companion object {
+        val A = 0
+    }
     override fun setViewModel() {
-
+        viewModel = ViewModelProvider(this)[PictureViewModel::class.java]
+        viewModel.activity = this
     }
 
     override fun setBinding(lf: LayoutInflater, ct: ViewGroup?): FragmentPictureBinding =
         FragmentPictureBinding.inflate(lf, ct, false)
 
+    private val arr = mutableListOf<Long>()
+    private var count = 0L
     override fun initView() {
         binding.tv.setOnClickListener {
-            navController.navigate(PictureFragmentDirections.actionPictureFragmentToNameFragment())
+            Log.d(TAG, "tv")
+            viewModel.getString()
         }
+        binding.camera.setOnClickListener {
+            binding.tv.text = A.toString()
+        }
+        binding.clPostPicture.setOnClickListener {
+            Log.d(TAG, "post")
+            startFragment(PictureFragmentDirections.actionPictureFragmentToNameFragment())
+        }
+    }
+
+    private fun go() {
+        arr.add(count)
+        count += 1000000L
+
+        Log.d(TAG, arr.size.toString())
     }
 }
--- app/src/main/java/com/hyunjine/petplant/view/post/picture/vm/PictureViewModel.kt
+++ app/src/main/java/com/hyunjine/petplant/view/post/picture/vm/PictureViewModel.kt
@@ -1,6 +1,36 @@
 package com.hyunjine.petplant.view.post.picture.vm
 
+import android.app.Activity
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.hyunjine.petplant.common.TAG
 import com.hyunjine.petplant.common.base.BaseViewModel
+import com.hyunjine.petplant.common.util.Test
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.subjects.PublishSubject
 
 class PictureViewModel: BaseViewModel() {
+    var activity: Fragment? = null
+
+    private val compositeDisposables: CompositeDisposable by lazy { CompositeDisposable() }
+
+    fun addDisposables(disposable: Disposable) {
+        compositeDisposables.add(disposable)
+    }
+
+
+    private var value = "a"
+    val observer: PublishSubject<String> = PublishSubject.create()
+
+    fun getString() {
+        addDisposables(Test().start()
+            .subscribe {
+                Log.d(TAG, it)
+            })
+    }
+
+    fun detachView() {
+        compositeDisposables.dispose()
+    }
 }
