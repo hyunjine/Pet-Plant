d276752b659ebca07879b819075b7f234cd6d9be
hyunjine <hyunjin_98_dev@naver.com>
2022-08-11 20:50:23 +0900

    Make Custom Bottom AppBar

--- app/src/main/java/com/hyunjine/petplant/view/main/MainActivity.kt
+++ app/src/main/java/com/hyunjine/petplant/view/main/MainActivity.kt
@@ -1,35 +1,31 @@
 package com.hyunjine.petplant.view.main
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import com.hyunjine.petplant.R
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.hyunjine.petplant.common.base.BaseActivity
 import com.hyunjine.petplant.databinding.ActivityMainBinding
 
-
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setBinding()
-        initView()
+class MainActivity : BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inflate(it) }) {
+    override fun initVew() {
+        setView()
     }
 
-    private fun setBinding() = ActivityMainBinding.inflate(layoutInflater).also {
-        binding = it
-        setContentView(it.root)
-    }
+    private fun setView() = binding.run {
+        val params = bottomAppBar.layoutParams as CoordinatorLayout.LayoutParams
+        params.behavior = object : HideListenableBottomAppBarBehavior() {
+            override fun onSlideDown() {
+                imgCalendar.visibility = View.GONE
+                imgEtc.visibility = View.GONE
+                tvCalendar.visibility = View.GONE
+                tvEtc.visibility = View.GONE
+            }
 
-    private fun initView() {
-        setBottomNavigation()
-
-    }
-
-    private fun setBottomNavigation() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
+            override fun onSlideUp() {
+                imgCalendar.visibility = View.VISIBLE
+                imgEtc.visibility = View.VISIBLE
+                tvCalendar.visibility = View.VISIBLE
+                tvEtc.visibility = View.VISIBLE
+            }
+        }
     }
 }
